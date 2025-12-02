391763a9cd511f43f416ead716c7030d9d684503
ishwar_kalmegh <ishwarkalmegh156@gmail.com>
2025-12-02 22:19:11 +0530

    solved the first part

--- 2025/001day/main.go
+++ 2025/001day/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -12,5 +14,33 @@ func main() {
 		log.Println("error occured:", err)
 	}
 
-	fmt.Println(string(data))
+	complete_string := string(data)
+	trimed_string := strings.TrimSpace(complete_string)
+	positions := strings.Split(trimed_string, "\n")
+
+	fmt.Println(len(positions))
+
+	current_pos := 50
+	zero_reach_count := 0
+	for _, val := range positions {
+		dir := val[:1]
+		moves, err := strconv.Atoi(val[1:])
+		if err != nil {
+			log.Println("error occured while converting to number", err)
+		}
+		if dir == "L" {
+			current_pos = current_pos - moves
+		} else {
+			current_pos = current_pos + moves
+		}
+
+		current_pos = current_pos % 100
+
+		if current_pos == 0 {
+			zero_reach_count++
+		}
+
+	}
+
+	fmt.Println("zeroth position reached for ", zero_reach_count)
 }
